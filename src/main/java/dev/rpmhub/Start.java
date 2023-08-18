8ce14688f8685b0503beeb512e6a645bcc6e7b1d
Davi Bavaresco <daviantoniobavaresco@gmail.com>
2023-08-18 19:14:58 -0300

    Teste concluido

--- src/main/java/dev/rpmhub/Start.java
+++ src/main/java/dev/rpmhub/Start.java
@@ -1,7 +1,11 @@
 package dev.rpmhub;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -13,6 +17,25 @@ public class Start {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy";
+    }
+
+    @GET
+    @Path("/no2kmh/{km}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String no2kmh(@PathParam("km") Double km){
+        Double x= km*1.852;
+        String xx= String. valueOf(x);
+        return xx;
+    }
+
+    @POST
+    @Path("/kmh2mih")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String kmh2mih(@FormParam("kmh") Double kmh){
+        Double x=kmh*0.621;
+        String xx= String. valueOf(x);
+        return xx; 
     }
 
 }
